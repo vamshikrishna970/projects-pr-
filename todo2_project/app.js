4764b0c4756fb2ff9fae91714f78f691815ca18b
vamshi krishna <vk676434@gamil.com>
2026-01-13 01:01:09 +0530

    progran updates

--- todo2_project/app.js
+++ todo2_project/app.js
@@ -3,5 +3,29 @@ let ul = document.querySelector("ul");
 let input = document.querySelector("input");
 
 btn.addEventListener("click", function() {
-    console.log("clicked");
+    let item = document.createElement("li");
+    item.textContent = input.value;
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.className = "delete";
+
+
+
+
+    ul.appendChild(item);
+    item.appendChild(deleteBtn);
+    console.log(input.value);
+    input.value = "";
+
 });
+
+let deleteButtons = document.querySelectorAll(".delete");
+for(delbtn of deleteButtons){
+    delbtn.addEventListener("click", function(){
+     let par = this.parentElement;
+     console.log(par);
+        par.remove();
+    })
+
+}
