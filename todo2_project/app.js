e812d23223d88b4ff35b646719a3575dce323109
K.Vamshi Krishna <121361572+vamshikrishna970@users.noreply.github.com>
2026-01-13 00:24:26 +0530

    click btn add

---
+++ todo2_project/app.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let input = document.querySelector("input");
+
+btn.addEventListener("click", function() {
+    console.log("clicked");
+});
